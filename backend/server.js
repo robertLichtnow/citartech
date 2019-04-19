a06930e9f256515c5130a490a820251bdffb9ddd
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-04-19 10:43:59 -0300

    Adicionado código de boilerplate da aplicação

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app  = express();
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/paises');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+
+app.use('/', router)
+
+app.listen(4000, ()=>{
+    console.log('running on port 4000');
+})
